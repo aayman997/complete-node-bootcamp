fec9c9fbe96df292afd0a13025127114a3ff7586
Ahmed Ayman <ahmed.ayman@cegedim.com>
2022-01-16 00:04:28 +0200

    16.Parsing Variables from URLs
    Took 1 hour 11 minutes

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -30,19 +30,54 @@ const url = require('url');
 
 ////////////////////////////////////////////////
 /*    Server    */
+const replaceTemplate = (temp, product) => {
+	// let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+	if ( !product.organic )
+		output = output.replace(/{%NOTORGANIC%}/g, 'not-organic');
+	return output;
+};
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-	const pathName = req.url;
 
-	if ( pathName === '/' || pathName === '/overview' ) {
-		res.end('This is the OVERVIEW');
-	} else if ( pathName === '/product' ) {
-		res.end('This is the PRODUCT');
-	} else if ( pathName === '/api' ) {
+const server = http.createServer((req, res) => {
+	const baseURL = `http://${req.headers.host}/`;
+	const {searchParams, pathname} = new URL(req.url, baseURL);
+	// console.log(new URL(req.url, baseURL));
+	// console.log(searchParams, pathname);
+	// console.log(req.url);
+	// console.log(url.parse(req.url, true));
+
+	//  Overview page
+	if ( pathname === '/' || pathname === '/overview' ) {
+		res.writeHead(200, {'Content-type': 'text/html'});
+
+		const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('');
+		const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml);
+
+		res.end(output);
+		//  Product page
+	} else if ( pathname === '/product' ) {
+		res.writeHead(200, {'Content-type': 'text/html'});
+		const product = dataObject[searchParams.get('id')];
+		const output = replaceTemplate(tempProduct, product);
+		res.end(output);
+		//  API
+	} else if ( pathname === '/api' ) {
 		res.writeHead(200, {'Content-type': 'application/json'});
 		res.end(data);
+		//  Not found
 	} else {
 		res.writeHead(404, {
 			'Content-tpe': 'text/html',
