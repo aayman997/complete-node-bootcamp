685fc68456ee02b36d77b2a199bf10487247680b
Ahmed Ayman <ahmed.ayman@cegedim.com>
2022-01-15 13:16:59 +0200

    12.Routing
    Took 21 minutes

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ////////////////////////////////////////////////
 /*    Files    */
@@ -30,8 +31,19 @@ const http = require('http');
 ////////////////////////////////////////////////
 /*    Server    */
 const server = http.createServer((req, res) => {
-	console.log(req);
-	res.end('Hello from the server');
+	const pathName = req.url;
+
+	if ( pathName === '/' || pathName === '/overview' ) {
+		res.end('This is the OVERVIEW');
+	} else if ( pathName === '/product' ) {
+		res.end('This is the PRODUCT');
+	} else {
+		res.writeHead(404, {
+			'Content-tpe': 'text/html',
+			'my-own-header': 'Hello-world'
+		});
+		res.end('<h1>PAGE NOT FOUND!</h1>');
+	}
 });
 
 server.listen(8000, '127.0.0.1', () => {
