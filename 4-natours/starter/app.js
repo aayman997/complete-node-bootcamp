f4e5d0ac0d77a36458c126dc6a99d0a5e9b6598f
Ahmed Ayman <ahmed.ayman@cegedim.com>
2022-01-19 09:29:51 +0200

    56. Handling DELETE Requests
    Took 55 minutes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
-
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({
@@ -25,6 +25,76 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours,
     },
+  });
+});
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
