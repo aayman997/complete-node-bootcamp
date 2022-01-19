eba6ddbb1a424121be1c75a86ab4c6da3d1515c3
Ahmed Ayman <ahmed.ayman@cegedim.com>
2022-01-19 13:19:24 +0200

    60. Using 3rd-Party Middleware
    Took 1 hour 3 minutes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
+
 const app = express();
+
+//  1- Middlewares
+app.use(morgan('dev'));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//  2- Route Handlers
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -87,12 +106,7 @@ const deleteTour = (req, res) => {
   });
 };
 
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
+//  3- Routes
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
@@ -100,6 +114,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+//  4- Start Server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}...`);
