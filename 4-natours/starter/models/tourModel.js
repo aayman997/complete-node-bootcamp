32950af21fa02a541f507de79ec6a988443051a1
Ahmed Ayman <ahmed.ayman@cegedim.com>
2022-01-30 14:08:10 +0200

    105. Query Middleware
    Took 37 minutes

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const Console = require('console');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -55,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: {
@@ -71,7 +76,6 @@ tourSchema.virtual('durationWeeks').get(function () {
 });
 
 //  Document middleware: Runs before .save() and .create()
-
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
@@ -86,6 +90,22 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+//  Query middleware
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
