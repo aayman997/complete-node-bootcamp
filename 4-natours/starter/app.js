5f555c3d211a030f53d94b06c2e4e76abfbacb45
Ahmed Ayman <ahmed.ayman@cegedim.com>
2022-02-02 09:16:36 +0200

    111. Handling Unhandled Routes
    Took 29 minutes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -12,10 +12,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -25,6 +21,13 @@ app.use((req, res, next) => {
 //  3- Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 //  4- Start Server
 module.exports = app;
